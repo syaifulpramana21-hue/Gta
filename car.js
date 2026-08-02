f5c1a588175a370d180528a37452323bf7348f89
SJP.XD <syaifulpramana21@gmail.com>
2026-08-03 05:16:37 +0800

    Update car.js

--- car.js
+++ car.js
@@ -22,68 +22,17 @@ const cars = [
 
     function updateCars(dt){
 
-    for(const car of cars){
+    ...
 
-        if(!car.driver) continue;
+}
 
-        if(keys["w"]){
-            car.speed+=car.accel*dt;
-        }
+function drawCars(ctx){
 
-        if(keys["s"]){
-            car.speed-=car.brake*dt;
-        }
-
-        if(keys["a"]){
-            car.angle-=car.turn*dt;
-        }
-
-        if(keys["d"]){
-            car.angle+=car.turn*dt;
-        }
-
-        car.speed*=0.98;
-
-        if(car.speed>car.maxSpeed)
-            car.speed=car.maxSpeed;
-
-        if(car.speed<-car.maxSpeed/2)
-            car.speed=-car.maxSpeed/2;
-
-        car.x+=Math.cos(car.angle)*car.speed*dt;
-        car.y+=Math.sin(car.angle)*car.speed*dt;
-
-    }
-
-    }
     for(const car of cars){
 
         // badan mobil
-        ctx.fillStyle=car.color;
-        ctx.fillRect(
-            car.x-car.width/2,
-            car.y-car.height/2,
-            car.width,
-            car.height
-        );
-
-        // kaca
-        ctx.fillStyle="#90caf9";
-        ctx.fillRect(
-            car.x-18,
-            car.y-10,
-            36,
-            20
-        );
-
-        // roda
-        ctx.fillStyle="#111";
-
-        ctx.fillRect(car.x-28,car.y-18,10,6);
-        ctx.fillRect(car.x+18,car.y-18,10,6);
-        ctx.fillRect(car.x-28,car.y+12,10,6);
-        ctx.fillRect(car.x+18,car.y+12,10,6);
+        ...
 
     }
 
-          }
+}
